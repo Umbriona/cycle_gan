fixed issue with gradien penalty and normalisation in models

scripts/train_cyclegan.py
@@ -22,7 +22,7 @@ from utils import models_classifyer as models_class
 from utils import callbacks
 from utils import preprocessing as pre
 
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 parser = argparse.ArgumentParser(""" """)
 
@@ -37,7 +37,7 @@ args = parser.parse_args()
 
 
 def train(config, model, data, time, classifyer):
-    print(model.lambda_cycle)
+
     #file writers
     result_dir = os.path.join(config['Results']['base_dir'],time)
     
@@ -60,6 +60,7 @@ def train(config, model, data, time, classifyer):
     temp_diff_summary_hist_x = tf.summary.create_file_writer(os.path.join(base_dir,'temp_diff_hist_x'))
     temp_diff_summary_hist_y = tf.summary.create_file_writer(os.path.join(base_dir,'temp_diff_hist_y'))
     
+    weights_writer = tf.summary.create_file_writer(os.path.join(base_dir,'weights'))
     
     loss_writer_id = tf.summary.create_file_writer(os.path.join(base_dir,'loss_param_id'))
     loss_writer_cycle = tf.summary.create_file_writer(os.path.join(base_dir,'loss_param_cycle'))
@@ -90,12 +91,16 @@ def train(config, model, data, time, classifyer):
         
         #Anneal schedule for gumbel
         if config['CycleGan']['Generator']['use_gumbel']:
-            tf.keras.backend.set_value(model.G.gms.tau,    max(0.2, np.exp(-0.01*epoch)))
-            tf.keras.backend.set_value(model.F.gms.tau,    max(0.2, np.exp(-0.01*epoch)))
+            gms_g = model.G.get_layer(index=-1)
+            gms_f = model.F.get_layer(index=-1)
+            tf.keras.backend.set_value(gms_g.tau,    max(0.1, np.exp(-0.01*epoch)))
+            tf.keras.backend.set_value(gms_f.tau,    max(0.1, np.exp(-0.01*epoch)))
                 
         for step, x in enumerate(zip(batches_x,batches_y)):
-            losses_, logits = model.train_step( batch_data = x)
-
+            if config['CycleGan']['Discriminator']['use_gp']:
+                losses_, logits = model.train_step_gp( batch_data = x)
+            else:
+                losses_, logits = model.train_step( batch_data = x)
             metrics['loss_G'](losses_["Gen_G_loss"]) 
             metrics['loss_cycle_x'](losses_["Cycle_X_loss"])
             metrics['loss_disc_y'](losses_["Disc_X_loss"])
@@ -149,17 +154,16 @@ def train(config, model, data, time, classifyer):
                     stack_temp_diff_y = tf.concat([stack_temp_diff_y, temp_diff_y], axis=0)
                     stack_temp_diff_x = tf.concat([stack_temp_diff_x, temp_diff_x], axis=0)
                 
+                
+            with temp_diff_summary_x.as_default():
+                tf.summary.scalar('temp_diff', tf.math.reduce_mean(stack_temp_diff_x), step=epoch, description = 'temp_diff_x')
+            with temp_diff_summary_y.as_default():
+                tf.summary.scalar('temp_diff', tf.math.reduce_mean(stack_temp_diff_y), step=epoch, description = 'temp_diff_y')
 
-                
-                with temp_diff_summary_x.as_default():
-                    tf.summary.scalar('temp_diff', tf.math.reduce_mean(stack_temp_diff_x), step=epoch, description = 'temp_diff_x')
-                with temp_diff_summary_y.as_default():
-                    tf.summary.scalar('temp_diff', tf.math.reduce_mean(stack_temp_diff_y), step=epoch, description = 'temp_diff_y')
-                
-                with temp_diff_summary_hist_x.as_default():
-                    tf.summary.histogram('temp_diff_hist',stack_temp_diff_x, step=epoch)
-                with temp_diff_summary_hist_y.as_default():
-                    tf.summary.histogram('temp_diff_hist',stack_temp_diff_y, step=epoch)
+            with temp_diff_summary_hist_x.as_default():
+                tf.summary.histogram('temp_diff_hist',stack_temp_diff_x, step=epoch)
+            with temp_diff_summary_hist_y.as_default():
+                tf.summary.histogram('temp_diff_hist',stack_temp_diff_y, step=epoch)
                     
         if epoch % 10 == 0:
             val_x = data['meso_val'].batch(32, drop_remainder=False)
@@ -171,36 +175,49 @@ def train(config, model, data, time, classifyer):
                 print(seq[:100])
             pass
 
-        print("G tau", tf.keras.backend.get_value(model.G.gms.tau))
-        print("F tau", tf.keras.backend.get_value(model.G.gms.tau))
+
         if args.verbose:    
-            print("Epoch: %d Loss_G: %2.4f Loss_F: %2.4f Loss_cycle_X: %2.4f Loss_cycle_Y: %2.4f Loss_D_Y: %2.4f Loss_D_X %2.4f tau %1.4f" % 
+            print("Epoch: %d Loss_G: %2.4f Loss_F: %2.4f Loss_cycle_X: %2.4f Loss_cycle_Y: %2.4f Loss_D_Y: %2.4f Loss_D_X %2.4f" % 
               (epoch, float(metrics['loss_G'].result()),
                float(metrics['loss_F'].result()),
                float(metrics['loss_cycle_x'].result()),
                float(metrics['loss_cycle_y'].result()),
                float(metrics['loss_disc_y'].result()),
-               float(metrics['loss_disc_x'].result()),
-               model.G.gms.tau))               
+               float(metrics['loss_disc_x'].result())))               
             print("Epoch: %d acc trans x: %2.4f acc trans y: %2.4f acc cycled x : %2.4f acc cycled y: %2.4f" % 
               (epoch, metrics['acc_x'].result(),
                metrics['acc_y'].result(),
                metrics['cycled_acc_x'].result(),
                metrics['cycled_acc_y'].result()))
+            
+            gms_g = model.G.get_layer(index=-1)
+            gms_f = model.F.get_layer(index=-1)
+            print("G tau %1.4f F tau %1.4f" % (tf.keras.backend.get_value(gms_g.tau), tf.keras.backend.get_value(gms_f.tau)))
+            
         
         # set Id
         if float(metrics['acc_x'].result()) > 0.8:
-            lambda_cycle = tf.keras.backend.get_value(model.lambda_cycle)
-            lambda_id = tf.keras.backend.get_value(model.lambda_id)
-            tf.keras.backend.set_value(model.lambda_id,  lambda_id * 0.5)
-            print(lambda_id)
+            lambda_cycle_G = tf.keras.backend.get_value(model.lambda_cycle_G)
+            lambda_id_G = tf.keras.backend.get_value(model.lambda_id_G)    
+            tf.keras.backend.set_value(model.lambda_id_G,  max(lambda_id_G * 0.5, 0.000001))
+            tf.keras.backend.set_value(model.lambda_cycle_G,  max(lambda_cycle_G * 0.95, 0.1))
+            print("lambda id G", lambda_id_G)
             #tf.keras.backend.set_value(model.lambda_cycle, lambda_cycle *0.7)
-
+        elif float(metrics['acc_x'].result()) < 0.7:  
+            tf.keras.backend.set_value(model.lambda_id_G,  config['CycleGan']['lambda_id'])
+            tf.keras.backend.set_value(model.lambda_cycle_G, config['CycleGan']['lambda_cycle'])
+                  
+        if float(metrics['acc_y'].result()) > 0.8:
+            lambda_cycle_F = tf.keras.backend.get_value(model.lambda_cycle_F)
+            lambda_id_F = tf.keras.backend.get_value(model.lambda_id_F)    
+            tf.keras.backend.set_value(model.lambda_id_F,  max(lambda_id_F * 0.5, 0.000001))
+            tf.keras.backend.set_value(model.lambda_cycle_F,  max(lambda_cycle_F * 0.95, 0.1))
+            print("Lambda id F", lambda_id_F)
+        elif float(metrics['acc_y'].result()) < 0.7:  
+            tf.keras.backend.set_value(model.lambda_id_F,  config['CycleGan']['lambda_id'])
+            tf.keras.backend.set_value(model.lambda_cycle_F, config['CycleGan']['lambda_cycle'])
                     
-        elif float(metrics['acc_x'].result()) < 0.7:
-            
-            tf.keras.backend.set_value(model.lambda_id,  config['CycleGan']['lambda_id'])
-            tf.keras.backend.set_value(model.lambda_cycle, config['CycleGan']['lambda_cycle'])
+        
             
         # Write log file
         with G_summary_writer.as_default():
@@ -213,27 +230,40 @@ def train(config, model, data, time, classifyer):
             tf.summary.scalar('acc', metrics['acc_y'].result(), step = epoch, description = 'Y transform' )
 
         with D_x_summary_writer.as_default():         
-            tf.summary.scalar('loss', metrics['loss_disc_y'].result(), step = epoch, description = 'X discriminator')        
+            tf.summary.scalar('loss_discriminator', metrics['loss_disc_y'].result(), step = epoch, description = 'X discriminator')        
         with D_y_summary_writer.as_default():        
-            tf.summary.scalar('loss', metrics['loss_disc_x'].result(), step = epoch, description = 'Y discriminator')    
+            tf.summary.scalar('loss_discriminator', metrics['loss_disc_x'].result(), step = epoch, description = 'Y discriminator')    
         with X_c_summary_writer.as_default(): 
-            tf.summary.scalar('loss', metrics['loss_cycle_x'].result(), step = epoch, description = 'X cycle')
+            tf.summary.scalar('loss_cycle', metrics['loss_cycle_x'].result(), step = epoch, description = 'X cycle')
             tf.summary.scalar('acc', metrics['cycled_acc_x'].result(), step = epoch, description = 'X cycle' )         
         with Y_c_summary_writer.as_default():
-            tf.summary.scalar('loss', metrics['loss_cycle_y'].result(), step = epoch, description = 'Y cycle')
+            tf.summary.scalar('loss_cycle', metrics['loss_cycle_y'].result(), step = epoch, description = 'Y cycle')
             tf.summary.scalar('acc', metrics['cycled_acc_y'].result(), step = epoch, description = 'Y cycle' )
             
         with X_id_summary_writer.as_default(): 
-            tf.summary.scalar('loss', metrics['loss_id_x'].result(), step = epoch, description = 'X Id')
+            tf.summary.scalar('loss_cycle', metrics['loss_id_x'].result(), step = epoch, description = 'X Id')
             tf.summary.scalar('acc', metrics['id_acc_x'].result(), step = epoch, description = 'X Id' )         
         with Y_id_summary_writer.as_default():
-            tf.summary.scalar('loss', metrics['loss_id_y'].result(), step = epoch, description = 'Y Id')
+            tf.summary.scalar('loss_cycle', metrics['loss_id_y'].result(), step = epoch, description = 'Y Id')
             tf.summary.scalar('acc', metrics['id_acc_y'].result(), step = epoch, description = 'Y Id' )
 
         with loss_writer_cycle.as_default():
-            tf.summary.scalar('lambda', tf.keras.backend.get_value(model.lambda_cycle),step = epoch, description = 'Cycle')
+            tf.summary.scalar('lambda', tf.keras.backend.get_value(model.lambda_cycle_G),step = epoch, description = 'Cycle')
         with loss_writer_id.as_default():
-            tf.summary.scalar('lambda', tf.keras.backend.get_value(model.lambda_id),step = epoch, description = 'Id')
+            tf.summary.scalar('lambda', tf.keras.backend.get_value(model.lambda_id_G),step = epoch, description = 'Id')
+        
+        with weights_writer.as_default():
+            for idx_layer, layer in enumerate( tf.keras.backend.get_value(model.G.trainable_variables)): 
+                tf.summary.histogram("G_{}".format(layer.name), layer.numpy(), step=epoch, description = 'G weights')
+        with weights_writer.as_default():
+            for idx_layer, layer in enumerate( tf.keras.backend.get_value(model.F.trainable_variables)): 
+                tf.summary.histogram("F_{}".format(layer.name), layer.numpy(), step=epoch, description = 'F weights')
+        with weights_writer.as_default():
+            for idx_layer, layer in enumerate( tf.keras.backend.get_value(model.D_x.trainable_variables)): 
+                tf.summary.histogram("D_x_{}".format(layer.name), layer.numpy(), step=epoch, description = 'D x weights')
+        with weights_writer.as_default():
+            for idx_layer, layer in enumerate( tf.keras.backend.get_value(model.D_y.trainable_variables)): 
+                tf.summary.histogram("D_y_{}".format(layer.name), layer.numpy(), step=epoch, description = 'D y weights')
             
         # Save history object
         history["Gen_G_loss"].append(metrics['loss_G'].result().numpy())
@@ -303,15 +333,15 @@ def main():
     model.compile(loss_obj, optimizers)
     
     # load classifyer
-    classifyer = models_weights = ["../weights/Model1/model1_weights.h5", "../weights/Model3/model3_weights.h5", "../weights/Model5/model5_weights.h5"]
+    models_weights = ["../weights/NEW_MODELS/Model1/variables/variables", "../weights/NEW_MODELS/Model2/variables/variables", "../weights/NEW_MODELS/Model3/variables/variables"]
     names = ["model1", "model2", "model3"]
     file = "../config/Classifier/config_classifier1.yaml"
     with open(file, 'r') as file_descriptor:
         config_class = yaml.load(file_descriptor, Loader=yaml.FullLoader)
     model_input = tf.keras.layers.Input(shape=(512,21))
-    model1 = models_class.Classifier_reg1(config_class['Classifier'], name = names[0])
-    model2 = models_class.Classifier_reg1(config_class['Classifier'], name = names[1])
-    model3 = models_class.Classifier_reg1(config_class['Classifier'], name = names[2])
+    model1 = models_class.get_classifier(config_class['Classifier'], 21)
+    model2 = models_class.get_classifier(config_class['Classifier'], 21)
+    model3 = models_class.get_classifier(config_class['Classifier'], 21)
 
     output1 = model1(model_input)
     output2 = model2(model_input)
@@ -319,10 +349,9 @@ def main():
 
     model1.summary()
 
-    model1.load_weights("../weights/Model1/model1_weights.h5")
-    model2.load_weights("../weights/Model3/model3_weights.h5")
-    model3.load_weights("../weights/Model5/model5_weights.h5")
-
+    model1.load_weights(models_weights[0]).expect_partial()
+    model2.load_weights(models_weights[1]).expect_partial()
+    model3.load_weights(models_weights[2]).expect_partial()
 
     ensemble_output = tf.keras.layers.Average()([output1, output2, output3])
     ensemble_model = tf.keras.Model(inputs=model_input, outputs=ensemble_output)

utils/layers_residual.py
@@ -11,11 +11,9 @@ def linear(x):
     return x
 
 class Linear(Layer):
-    def __init__(self, obj, name):
-        super(Linear, self).__init__(name=name)
-        self.obj = obj
-    def call(x):
-        x = self.obj(x)
+    def __init__(self):
+        super(Linear, self).__init__()
+    def call(self, x):
         return x
 
 class ResMod_old(Layer):
@@ -353,6 +351,83 @@ def residual_mod(filters,
         result.add(tfa.layers.InstanceNormalization())
     elif norm == "Layer":
         result.add(LayerNormalization())
+    else:
+        result.add(Linear())
+    # first activation
+    if act=="LReLU":
+        result.add(LeakyReLU())
+    else:
+        result.add(ReLU())
+        
+    # first conv
+    if use_dout:
+        result.add(Dropout(0.2))
+    if sn:
+        result.add(SpectralNormalization(Conv1D(filters,
+                      size,
+                      padding = 'same',
+                      use_bias = True,
+                      kernel_regularizer = L1L2(l1=l1, l2=l2))))
+    else:
+        result.add(Conv1D(filters,
+                      size,
+                      padding = 'same',
+                      use_bias = True,
+                      kernel_regularizer = L1L2(l1=l1, l2=l2)))
+    #second norm   
+    if norm == "Batch":
+        result.add(BatchNormalization())
+    elif norm == "Instance":
+        result.add(tfa.layers.InstanceNormalization())
+    elif norm == "Layer":
+        result.add(LayerNormalization())
+    else:
+        result.add(Linear())
+    # second activation
+    if act=="LReLU":
+        result.add(LeakyReLU())
+    else:
+        result.add(ReLU())
+        
+    # second conv
+    if use_dout:
+        result.add(Dropout(0.2))
+    if sn:
+        result.add(SpectralNormalization(Conv1D(filters,
+                      size,
+                      padding = 'same',
+                      use_bias = True,
+                      kernel_regularizer = L1L2(l1=l1, l2=l2))))
+    else:
+        result.add(Conv1D(filters,
+                      size,
+                      padding = 'same',
+                      use_bias = True,
+                      kernel_regularizer = L1L2(l1=l1, l2=l2)))
+    return result
+    
+    
+def residual_mod_new(filters,
+                 size,
+                 strides=1,
+                 dilation=1,
+                 l1=0.0, l2=0.0,
+                 rate = 0.2,
+                 use_bias=False,
+                 norm="Instance",
+                 sn = False,
+                 use_dout=False,
+                 act="LReLU", name = "res"):
+    
+    result = tf.keras.Sequential()
+    
+    # first normalisation
+    if norm == "Batch":
+        result.add(BatchNormalization())
+    elif norm == "Instance":
+        result.add(tfa.layers.InstanceNormalization())
+    elif norm == "Layer":
+        result.add(LayerNormalization())
     
     # first activation
     if act=="LReLU":
@@ -367,13 +442,13 @@ def residual_mod(filters,
         result.add(SpectralNormalization(Conv1D(filters,
                       size,
                       padding = 'same',
-                      use_bias = False,
+                      use_bias = True,
                       kernel_regularizer = L1L2(l1=l1, l2=l2))))
     else:
         result.add(Conv1D(filters,
                       size,
                       padding = 'same',
-                      use_bias = False,
+                      use_bias = True,
                       kernel_regularizer = L1L2(l1=l1, l2=l2)))
     #second norm   
     if norm == "Batch":
@@ -396,13 +471,12 @@ def residual_mod(filters,
         result.add(SpectralNormalization(Conv1D(filters,
                       size,
                       padding = 'same',
-                      use_bias = False,
+                      use_bias = True,
                       kernel_regularizer = L1L2(l1=l1, l2=l2))))
     else:
         result.add(Conv1D(filters,
                       size,
                       padding = 'same',
-                      use_bias = False,
+                      use_bias = True,
                       kernel_regularizer = L1L2(l1=l1, l2=l2)))
     return result
-    

utils/losses.py
@@ -7,12 +7,19 @@ class WassersteinLoss(Loss):
     def __init__(self, ):
         super(WassersteinLoss, self).__init__()
         self.cross = CategoricalCrossentropy(from_logits=False, label_smoothing=0.0)
-    def cycle_loss_fn(self, real, cycled, w):
+        
+    def cycle_loss_fn(self, real, cycled, w=None):
+        #return tf.reduce_mean(tf.abs(real - cycled))
         return self.cross( real, cycled, w)
+    
+    def identity_loss_fn(self, real, same, w = None):
+        #loss = tf.reduce_mean(tf.abs(real - same))
+        loss = self.cross( real, same, w)
+        return loss
     
     # Define the loss function for the generators
     def generator_loss_fn(self, fake):
-        return -tf.reduce_mean(fake, axis=0)
+        return -tf.reduce_mean(fake)
 
     # Define the loss function for the discriminators
     def discriminator_loss_fn(self, real, fake):
@@ -24,19 +31,31 @@ class NonReduceingLoss(Loss):
     def __init__(self, ):
         super(NonReduceingLoss, self).__init__()
         self.cross = CategoricalCrossentropy(from_logits=False, label_smoothing=0.0)#, reduction=tf.keras.losses.Reduction.NONE)
-    def cycle_loss_fn(self, real, cycled, w):
-        return self.cross( real, cycled, w) #tf.reduce_mean(self.cross( real, cycled, w), axis=0)
+        self.bin = loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        
+    def cycle_loss_fn(self, real, cycled, w=None):
+        #return tf.reduce_mean(tf.abs(real - cycled))
+        return self.cross( real, cycled, w)
+    
+    def identity_loss_fn(self, real, same, w = None):
+        #loss = tf.reduce_mean(tf.abs(real - same))
+        loss = self.cross( real, same, w)
+        return loss
     
     def generator_loss_fn(self, fake):
-        #return -tf.math.reduce_mean(tf.math.log(fake), axis=None)
-        return K.mean(K.softplus(-fake), axis=0)
+        return self.bin(tf.ones_like(fake), fake)
+        #return K.mean(K.softplus(-fake), axis=0)
     
     def discriminator_loss_fn(self, real, fake):
         #L1 = tf.reduce_mean(tf.math.log(real))
         #L2 = tf.reduce_mean(tf.math.log(tf.ones_like(fake)-fake))
-        L1 = K.mean(K.softplus(-real), axis=0)
-        L2 = K.mean(K.softplus(fake), axis=0)
-        return L1+L2#-1*(L1 + L2) #* (-1)
+        #L1 = K.mean(K.softplus(-real), axis=0)
+        #L2 = K.mean(K.softplus(fake), axis=0)
+        # total_disc_loss = L1+L2
+        real_loss = self.bin(tf.ones_like(real), real)
+        generated_loss = self.bin(tf.zeros_like(fake), fake)
+        total_disc_loss = real_loss + generated_loss
+        return total_disc_loss * 0.5
     
 class HingeLoss(Loss):
     def __init__(self ):

utils/models_cyclegan.py
@@ -30,8 +30,10 @@ class CycleGan(tf.keras.Model):
         self.D_y.summary()
         
 
-        self.lambda_cycle = tf.Variable(config['CycleGan']['lambda_cycle'], dtype=tf.float32, trainable=False)
-        self.lambda_id    = tf.Variable(config['CycleGan']['lambda_id'], dtype=tf.float32, trainable=False) 
+        self.lambda_cycle_G = tf.Variable(config['CycleGan']['lambda_cycle'], dtype=tf.float32, trainable=False)
+        self.lambda_id_G    = tf.Variable(config['CycleGan']['lambda_id'], dtype=tf.float32, trainable=False) 
+        self.lambda_cycle_F = tf.Variable(config['CycleGan']['lambda_cycle'], dtype=tf.float32, trainable=False)
+        self.lambda_id_F    = tf.Variable(config['CycleGan']['lambda_id'], dtype=tf.float32, trainable=False) 
         self.add  = tf.keras.layers.Add()
         
     def compile( self, loss_obj, optimizers):
@@ -45,7 +47,7 @@ class CycleGan(tf.keras.Model):
         self.generator_loss_fn = loss_obj.generator_loss_fn
         self.discriminator_loss_fn = loss_obj.discriminator_loss_fn
         self.cycle_loss_fn = loss_obj.cycle_loss_fn
-        self.identity_loss_fn = loss_obj.cycle_loss_fn
+        self.id_loss_fn = loss_obj.identity_loss_fn
         
     def load_models(self, config):
         """Create all models that is used in cycle gan""" 
@@ -56,11 +58,16 @@ class CycleGan(tf.keras.Model):
             D_activation = 'linear'
 
         vocab = config["Vocab_size"] 
+        
+        G    = models_gen.get_generator(config["Generator"], vocab)
+        F    = models_gen.get_generator(config["Generator"], vocab)
+        D_x  = models_dis.get_discriminator(config["Discriminator"], vocab, activation=D_activation)
+        D_y  = models_dis.get_discriminator(config["Discriminator"], vocab, activation=D_activation)
 
-        G    = models_gen.Generator_res(config["Generator"], vocab, name = "Generator_thermo")
-        F    = models_gen.Generator_res(config["Generator"], vocab, name = "Generator_meso") 
-        D_x  = models_dis.Discriminator(config["Discriminator"], vocab, activation = D_activation, name = "Discriminator_thermo")
-        D_y  = models_dis.Discriminator(config["Discriminator"], vocab, activation = D_activation, name = "Discriminator_meso")
+        #G    = models_gen.Generator_res(config["Generator"], vocab, name = "Generator_thermo")
+        #F    = models_gen.Generator_res(config["Generator"], vocab, name = "Generator_meso") 
+        #D_x  = models_dis.Discriminator(config["Discriminator"], vocab, activation = D_activation, name = "Discriminator_thermo")
+        #D_y  = models_dis.Discriminator(config["Discriminator"], vocab, activation = D_activation, name = "Discriminator_meso")
 
         return G, F, D_x, D_y
     
@@ -100,69 +107,50 @@ class CycleGan(tf.keras.Model):
         return gp_y, gp_x
     
     @tf.function
-    def train_step(self, batch_data):
-
-        X_bin, _, W_x= batch_data[0]
-        Y_bin, _, W_y= batch_data[1]
+    def train_step_gp(self, batch_data):
 
         with tf.GradientTape(persistent=True) as tape:
-            X_bin, _, W_x = batch_data[0]
-            Y_bin, _, W_y= batch_data[1]
-            #print("X_bin", X_bin)
+            real_x, _, W_x = batch_data[0]
+            real_y, _, W_y= batch_data[1]
             
-            fake_y = self.G(X_bin, training=True)
-            fake_x = self.F(Y_bin, training=True)
-            
-            #Apply mask
+            # Calculate masks to perserve padding in fake sequences shape (None, 512, 1) -> (None, 512, 21)
             mask_x = tf.repeat(W_x, 21, axis=-1)
             mask_y = tf.repeat(W_y, 21, axis=-1)
-                
-            fake_y = tf.math.multiply(fake_y, mask_x)
-            fake_x = tf.math.multiply(fake_x, mask_y)
             
-            #print("Fake y", fake_y)
-            #print("Fake x", fake_x)
+            fake_y = self.G(real_x, training=True)   # G:x -> y'
+            fake_y = tf.math.multiply(fake_y, mask_x) # Preserve padding
+            cycled_x = self.F(fake_y, training=True) # Cycle: F:y' -> x
             
+            fake_x = self.F(real_y, training=True)   # F:y -> x'
+            fake_x = tf.math.multiply(fake_x, mask_y) ##Apply mask
+            cycled_y = self.G(fake_x, training=True) # Cycle: G:x' -> y        
+
             # Identity mapping
-            same_x = self.F(X_bin, training=True)
-            same_y = self.G(Y_bin, training=True)
-            #print("same_x", same_x)
-            # Cycle: x -> y -> x
-            cycled_x = self.F(fake_y, training=True)
-            cycled_y = self.G(fake_x, training=True)
-            #print("cycled_x", cycled_x)
+            same_x = self.F(real_x, training=True)  #F:x -> x
+            same_y = self.G(real_y, training=True)  #G:y -> y
             
-            # Discriminator output
-            disc_real_y = self.D_y(Y_bin, training=True)
-            disc_fake_y = self.D_y(fake_y, training=True)
-            #print("disc_real x", disc_real_x)
-            #print("disc_fake x", disc_fake_x)
-        
-            disc_real_x = self.D_x(X_bin, training=True)
+            disc_real_x = self.D_x(real_x, training=True)
+            disc_real_y = self.D_y(real_y, training=True)
+            
             disc_fake_x = self.D_x(fake_x, training=True)
-            #print("disc_real y", disc_real_y)
-            #print("disc_fake y", disc_fake_y)
+            disc_fake_y = self.D_y(fake_y, training=True)
 
             gen_G_loss = self.generator_loss_fn(disc_fake_y)
             gen_F_loss = self.generator_loss_fn(disc_fake_x)
-            #print('Loss G:', gen_G_loss)
 
-            id_G_loss = self.cycle_loss_fn(Y_bin, same_y, W_y)  * self.lambda_cycle * self.lambda_id
-            id_F_loss = self.cycle_loss_fn(X_bin, same_x, W_x)  * self.lambda_cycle * self.lambda_id
-            #print('Id loss G:', id_G_loss)
+            id_G_loss = self.id_loss_fn(real_y, same_y, W_y)  * self.lambda_cycle_G * self.lambda_id_G
+            id_F_loss = self.id_loss_fn(real_x, same_x, W_x)  * self.lambda_cycle_G * self.lambda_id_F
             
-            gen_cycle_x_loss = self.cycle_loss_fn(X_bin, cycled_x, W_x)  * self.lambda_cycle 
-            gen_cycle_y_loss = self.cycle_loss_fn(Y_bin, cycled_y, W_y)  * self.lambda_cycle 
-            #print('C loss G', gen_cycle_x_loss)
-
+            cycle_G_loss = self.cycle_loss_fn(real_y, cycled_y, W_y)  * self.lambda_cycle_G 
+            cycle_F_loss = self.cycle_loss_fn(real_x, cycled_x, W_x)  * self.lambda_cycle_G 
+            cycle_tot_loss= cycle_G_loss + cycle_F_loss
 
             # Generator total loss
-            tot_loss_G = gen_G_loss  + gen_cycle_x_loss  + id_G_loss 
-            tot_loss_F = gen_F_loss  + gen_cycle_y_loss  + id_F_loss 
-            #print('total loss G', tot_loss_G)
+            tot_loss_G = gen_G_loss  + cycle_tot_loss  + id_G_loss 
+            tot_loss_F = gen_F_loss  + cycle_tot_loss  + id_F_loss 
             
             # Discriminator loss
-            gp_y, gp_x = self.gradient_penalty(fake_y, Y_bin, fake_x, X_bin)
+            gp_y, gp_x = self.gradient_penalty(fake_y, real_y, fake_x, real_x)
             #print("gp_y", gp_y)
             #print("gp_x", gp_x)
             loss_D_y = self.discriminator_loss_fn(disc_real_y, disc_fake_y) + gp_y * 10
@@ -194,11 +182,93 @@ class CycleGan(tf.keras.Model):
 
         return {
             "Gen_G_loss": gen_G_loss,
-            "Cycle_X_loss": gen_cycle_x_loss,
+            "Cycle_X_loss": cycle_G_loss,
             "Id_X_loss": id_G_loss,
             "Disc_X_loss": loss_D_x,
             "Gen_F_loss": gen_F_loss,
-            "Cycle_Y_loss": gen_cycle_y_loss,
+            "Cycle_Y_loss": cycle_F_loss,
+            "Id_Y_loss": id_F_loss,
+            "Disc_Y_loss": loss_D_y
+        }, ((fake_y, fake_x),(cycled_x, cycled_y), (same_x, same_y))
+    
+    @tf.function
+    def train_step(self, batch_data):
+
+        with tf.GradientTape(persistent=True) as tape:
+            real_x, _, W_x = batch_data[0]
+            real_y, _, W_y= batch_data[1]
+            
+            # Calculate masks to perserve padding in fake sequences shape (None, 512, 1) -> (None, 512, 21)
+            mask_x = tf.repeat(W_x, 21, axis=-1)
+            mask_y = tf.repeat(W_y, 21, axis=-1)
+            
+            fake_y = self.G(real_x, training=True)   # G:x -> y'
+            fake_y = tf.math.multiply(fake_y, mask_x) # Preserve padding
+            cycled_x = self.F(fake_y, training=True) # Cycle: F:y' -> x
+            
+            fake_x = self.F(real_y, training=True)   # F:y -> x'
+            fake_x = tf.math.multiply(fake_x, mask_y) ##Apply mask
+            cycled_y = self.G(fake_x, training=True) # Cycle: G:x' -> y        
+
+            # Identity mapping
+            same_x = self.F(real_x, training=True)  #F:x -> x
+            same_y = self.G(real_y, training=True)  #G:y -> y
+            
+            disc_real_x = self.D_x(real_x, training=True)
+            disc_real_y = self.D_y(real_y, training=True)
+            
+            disc_fake_x = self.D_x(fake_x, training=True)
+            disc_fake_y = self.D_y(fake_y, training=True)
+
+            gen_G_loss = self.generator_loss_fn(disc_fake_y)
+            gen_F_loss = self.generator_loss_fn(disc_fake_x)
+
+            id_G_loss = self.id_loss_fn(real_y, same_y, W_y)  * self.lambda_cycle_G * self.lambda_id_G
+            id_F_loss = self.id_loss_fn(real_x, same_x, W_x)  * self.lambda_cycle_G * self.lambda_id_F
+            
+            cycle_G_loss = self.cycle_loss_fn(real_y, cycled_y, W_y)  * self.lambda_cycle_G 
+            cycle_F_loss = self.cycle_loss_fn(real_x, cycled_x, W_x)  * self.lambda_cycle_G 
+            cycle_tot_loss= cycle_G_loss + cycle_F_loss
+            
+            # Generator total loss
+            tot_loss_G = gen_G_loss  + cycle_tot_loss  + id_G_loss 
+            tot_loss_F = gen_F_loss  + cycle_tot_loss  + id_F_loss 
+            
+            loss_D_y = self.discriminator_loss_fn(disc_real_y, disc_fake_y) 
+            loss_D_x = self.discriminator_loss_fn(disc_real_x, disc_fake_x) 
+            
+            #print("disc_real x", disc_real_x)
+            #print("disc_fake x", disc_fake_x)
+            #print("disc_real y", disc_real_y)
+            #print("disc_fake y", disc_fake_y)
+            #print('total loss D_X', loss_D_x)
+            #print('total loss D_Y', loss_D_y)
+            #print("shape msak x", tf.shape(mask_x))
+            #print("shape msak y", tf.shape(mask_y))
+                   
+        grads_G_gen = tape.gradient(tot_loss_G, self.G.trainable_variables)
+        grads_F_gen = tape.gradient(tot_loss_F, self.F.trainable_variables)
+
+        # Get the gradients for the discriminators
+        grads_disc_y = tape.gradient(loss_D_y, self.D_y.trainable_variables)
+        grads_disc_x = tape.gradient(loss_D_x, self.D_x.trainable_variables)
+
+        # Update the weights of the generators 
+        self.gen_G_optimizer.apply_gradients(zip(grads_G_gen, self.G.trainable_variables))  
+        self.gen_F_optimizer.apply_gradients(zip(grads_F_gen, self.F.trainable_variables))
+
+
+        # Update the weights of the discriminators
+        self.disc_Y_optimizer.apply_gradients(zip(grads_disc_y, self.D_y.trainable_variables))
+        self.disc_X_optimizer.apply_gradients(zip(grads_disc_x, self.D_x.trainable_variables))
+
+        return {
+            "Gen_G_loss": gen_G_loss,
+            "Cycle_X_loss": cycle_G_loss,
+            "Id_X_loss": id_G_loss,
+            "Disc_X_loss": loss_D_x,
+            "Gen_F_loss": gen_F_loss,
+            "Cycle_Y_loss": cycle_F_loss,
             "Id_Y_loss": id_F_loss,
             "Disc_Y_loss": loss_D_y
         }, ((fake_y, fake_x),(cycled_x, cycled_y), (same_x, same_y))

utils/models_discriminator.py
@@ -6,8 +6,11 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Layer, Conv1D, Dense, Flatten, Activation, Embedding, Input, LeakyReLU, LayerNormalization, BatchNormalization, Softmax, Dropout, Concatenate
 from tensorflow.keras.regularizers import L1L2
 from utils.layers_new import  SelfAttentionSN, SelfAttention, GumbelSoftmax, SpectralNormalization
-from utils.layers_residual import ResModPreAct, ResModPreActSN
+from utils.layers_residual import ResModPreAct, ResModPreActSN, residual_mod
 from utils import preprocessing as pre
+
+def linear(x):
+    return x
 
 class Linear(Layer):
     def __init__(self, obj, name):
@@ -133,4 +136,119 @@ class Discriminator(Model):
 def discriminator(config):
     
     
+    return model
+
+def get_discriminator(config, vocab, activation = 'sigmoid'):
+    # Input layer
+    model_input = tf.keras.layers.Input(shape=(512,21))
+    
+    n_layers = config['n_layers']
+    
+    l1 = config['l1']
+    l2 = config['l2']
+    atte_loc = config['attention_loc']
+    use_atte = config['use_attention']
+    filters = config['filters']
+    kernels = config['kernels']
+    dilation = config['dilations']
+    use_spectral_norm = config['use_spectral_norm']
+    strides = config['strides']
+    patch = config['patch']
+    norm=config['norm']
+
+    
+    # Parameters
+
+
+    
+    
+
+    filters_down = [64,128,128,256,256]
+    
+    if use_spectral_norm:
+        projection = SpectralNormalization(Conv1D(64, 9, padding = "same", activation = LeakyReLU(), use_bias = True,kernel_regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2)))
+        att = SelfAttentionSN(filters[atte_loc])
+        down = [SpectralNormalization(Conv1D(filters_down[i], 9,
+             strides=2,
+             padding="same",
+             activation = LeakyReLU(),
+             kernel_regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2))) for i in range(len(filters_down))]
+    
+        conv1 = SpectralNormalization(Conv1D(filters_down[0], 9, padding = 'same', activation = LeakyReLU()))
+        outconv1 = SpectralNormalization(Conv1D(filters_down[-1], 9, padding = 'same', activation = LeakyReLU()))
+        pro = [SpectralNormalization(Conv1D(filters[i], 1, use_bias=True)) for i in range(n_layers)]
+        if patch:
+            out = SpectralNormalization(Conv1D(1, 9, padding= "same", activation = activation, kernel_regularizer = L1L2(l1=l1, l2=l2)))      
+        else:
+            flatt = Flatten()
+            out = SpectralNormalization(Dense(1, activation= activation, use_bias = False, kernel_regularizer = L1L2(l1=l1, l2=l2)))
+    else:
+        projection = Conv1D(64, 9, padding = "same", activation = LeakyReLU(), use_bias = True,kernel_regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2))
+        att = SelfAttention(filters[atte_loc])
+        down = [Conv1D(filters_down[i], 9,
+             strides=2,
+             padding="same",
+             activation = LeakyReLU(),
+             kernel_regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2)) for i in range(len(filters_down))]
+    
+        conv1 = Conv1D(filters_down[0], 9, padding = 'same', activation = LeakyReLU())
+        outconv1 = Conv1D(filters_down[-1], 9, padding = 'same', activation = LeakyReLU())
+        
+        pro = [Conv1D(filters[i], 1, use_bias=True) for i in range(n_layers)]
+        if patch:
+            out = Conv1D(1, 9, padding= "same", activation = activation, kernel_regularizer = L1L2(l1=l1, l2=l2))      
+        else:
+            flatt = Flatten()
+            out = Dense(1, activation= activation, use_bias = True, kernel_regularizer = L1L2(l1=l1, l2=l2))
+        
+      
+    res = [residual_mod(filters[i],
+                        kernels[i],
+                        dilation=dilation[i],
+                        l1=l1,
+                        l2=l2,
+                        use_dout = False,
+                        use_bias=False,
+                        norm=norm,
+                        sn = use_spectral_norm,
+                        act="LReLU") for i in range(n_layers)]
+    
+
+    # Normalisations
+    if norm == "Layer":
+        norm_up_down = [LayerNormalization(axis = -1, epsilon = 1e-6) for i in range(len(filters_down)+1)]
+    elif norm == "Batch":
+        norm_up_down = [BatchNormalization() for i in range(len(filters_down)+1)]
+    elif norm == "Instance":
+        norm_up_down = [tfa.layers.InstanceNormalization() for i in range(len(filters_down)+1)]
+    else:
+        norm_up_down = [linear for i in range(len(filters_down)+1)]
+    
+    
+
+
+    x = model_input
+    x = projection(x)      
+    x = conv1(x)
+
+    down_count=0       
+    for i in range(n_layers):
+        x_out  = res[i](x)
+        x_pro  = pro[i](x)
+
+        x = tf.keras.layers.Add()([x_out, x_pro])
+        if i == atte_loc and use_atte:
+            x = att(x)[0]
+        if 2 == strides[i]:
+            x = down[down_count](x)
+            x = norm_up_down[down_count](x)
+            down_count+=1
+
+
+    x = outconv1(x)
+    x = norm_up_down[down_count](x)
+    if not patch:
+        x = flatt(x)
+    x = out(x)
+    model = tf.keras.Model(inputs=model_input, outputs=x)
     return model

utils/models_generator.py
@@ -10,6 +10,9 @@ from utils import preprocessing as pre
 
 ATTENTION_FEATURES = 512
 
+def linear(x):
+    return x
+
 def sn_up_down(config, name,down_sample, l1, l2, gms, filters_up, filters_down, vocab):
     projection = SpectralNormalization(Dense(filters_down[0],
                                      activation = None,
@@ -224,7 +227,7 @@ def get_generator(config, vocab):
     
 
     if use_spectral_norm:
-        projection = SpectralNormalization(Dense(64,activation = None, use_bias = False,kernel_regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2)))
+        projection = SpectralNormalization(Conv1D(64,9, padding='same', activation = 'relu', use_bias = True ,kernel_regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2)))
         att = SelfAttentionSN(filters[atte_loc])
         down = [SpectralNormalization(Conv1D(filters_down[i], 9,
                  strides=2,
@@ -240,7 +243,7 @@ def get_generator(config, vocab):
         outconv1 = SpectralNormalization(Conv1D(filters_up[-1],9,padding = 'same', activation = "relu"))
         outconv2 = SpectralNormalization(Conv1D(vocab, 9, padding = 'same', activation = gms))
     else:
-        projection = Dense(64,activation = None, use_bias = False,kernel_regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2))
+        projection = Conv1D(64,9, padding='same', activation = 'relu', use_bias = True ,kernel_regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2))
         att = SelfAttention(filters[atte_loc])
         down = [Conv1D(filters_down[i], 9,
                  strides=2,
@@ -254,7 +257,7 @@ def get_generator(config, vocab):
                                 kernel_regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2)) for i in range(down_sample)]
 
         outconv1 = Conv1D(filters_up[-1],9,padding = 'same', activation = "relu")
-        outconv2 = Conv1D(vocab, 9, padding = 'same', activation = gms)
+        outconv2 = Conv1D(vocab, 9, padding = 'same', activation = None)
       
     res = [residual_mod(filters[i],
                         kernels[i],
@@ -262,32 +265,34 @@ def get_generator(config, vocab):
                         l1=l1,
                         l2=l2,
                         use_dout = False,
-                        use_bias=False,
+                        use_bias=True,
                         norm=norm,
                         sn = use_spectral_norm,
                         act="ReLU") for i in range(n_layers)]
 
     # Normalisations
     if norm == "Layer":
-        norm_up_down = [LayerNormalization(axis = -1, epsilon = 1e-6) for i in range(down_sample*2)]
+        norm_up_down = [LayerNormalization(axis = -1, epsilon = 1e-6) for i in range(down_sample*2+1)]
     elif norm == "Batch":
-        norm_up_down = [BatchNormalization() for i in range(down_sample*2)]
+        norm_up_down = [BatchNormalization() for i in range(down_sample*2+1)]
     elif norm == "Instance":
-        norm_up_down = [tfa.layers.InstanceNormalization() for i in range(down_sample*2)]
-
+        norm_up_down = [tfa.layers.InstanceNormalization() for i in range(down_sample*2+1)]
+    else:
+        norm_up_down = [linear for i in range(down_sample*2+1)]
 
             
 
     skip = []
     x = model_input
-    x = projection(x)
-           
+    x = projection(x)       
     x = norm_up_down[0](x)
+    
     x = down[0](x)
+    x = norm_up_down[1](x) 
     skip.append(x)
-           
+    
     x = down[1](x)
-    x = norm_up_down[1](x)
+    x = norm_up_down[2](x)
     skip.append(x)
 
            
@@ -298,12 +303,13 @@ def get_generator(config, vocab):
             x = att(x)[0]
     x = tf.keras.layers.Concatenate()([skip[1], x])
     x = up[0](x)
-    x = norm_up_down[2](x)
+    x = norm_up_down[3](x)
     x = tf.keras.layers.Concatenate()([skip[0], x])
     x = up[1](x)
-    x = norm_up_down[3](x)  
+    x = norm_up_down[4](x)  
     x = outconv1(x)
     x = outconv2(x)
+    x = gms(x) 
     model = tf.keras.Model(inputs=model_input, outputs=x)
     return model
     